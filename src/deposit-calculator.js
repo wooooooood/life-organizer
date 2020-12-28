e9a16bede64075bfc4c8fa870e8a66b3426efb89
wooooooood <13pft13@gmail.com>
2020-12-28 22:05:02 +0900

    Refactor

--- src/deposit-calculator.js
+++ src/deposit-calculator.js
@@ -1,5 +1,6 @@
 export default function DepositCalculator(){
-    const $result = document.querySelector('#result');
+    const $savingsResult = document.querySelector('#savings-result');
+    const $depositResult = document.querySelector('#deposit-result');
     const $deposit = document.querySelector('#deposit');
     const $depositRate = document.querySelector('#deposit-rate');
     const $depositMonth = document.querySelector('#deposit-month');
@@ -8,11 +9,11 @@ export default function DepositCalculator(){
     const $savingsMonth = document.querySelector('#savings-month');
     const $submitButton = document.querySelector('#submit');
     $submitButton.addEventListener('click', (e) => {
-        $result.innerHTML = `
-        <ul>
-        <li>예금: ${$deposit.value*(1+$depositRate.value*$depositMonth.value/1200) }</li>
-        <li>적금: ${($savings.value*$savingsMonth.value) + ($savings.value * $savingsMonth.value*(parseInt($savingsMonth.value)+1)/2 * $savingsRate.value/12*0.01) }</li>
-        </ul>
+        $depositResult.innerHTML = `
+        <ul><li>예금만기 시: ${$deposit.value*(1+$depositRate.value*$depositMonth.value/1200) }</li></ul>
+        `;
+        $savingsResult.innerHTML = `
+        <ul><li>적금만기 시: ${($savings.value*$savingsMonth.value) + ($savings.value * $savingsMonth.value*(parseInt($savingsMonth.value)+1)/2 * $savingsRate.value/12*0.01) }</li></ul>
         `;
     });
 }
