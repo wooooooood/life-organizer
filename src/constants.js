18b671424b434b76700a2a40706b151968e79552
wooooooood <13pft13@gmail.com>
2020-12-29 23:16:36 +0900

    세율 상수 분리

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const TARIFF = {
+    GENERAL: 0.154, //일반과세
+    PREFERENTIAL: 0.095, //세금우대
+    NONE: 0, //비과세
+}
--- src/deposit-calculator.js
+++ src/deposit-calculator.js
@@ -1,3 +1,5 @@
+import {TARIFF} from './constants.js';
+
 export default function DepositCalculator(){
     const $savingsResult = document.querySelector('#savings-result');
     const $depositResult = document.querySelector('#deposit-result');
@@ -13,16 +15,16 @@ export default function DepositCalculator(){
         <ul>
             <li>원금: ${$deposit.value}</li>
             <li>세전이자: ${$deposit.value*($depositRate.value*$depositMonth.value/1200) }</li>
-            <li>일반과세(15.4%): ${Math.round($deposit.value*($depositRate.value*$depositMonth.value/1200) * 0.154)}</li>
-            <li>세후 수령액: ${Math.round($deposit.value*(1+$depositRate.value*$depositMonth.value/1200*0.846))}</li>
+            <li>일반과세(${TARIFF.GENERAL * 100}%): ${Math.round($deposit.value*($depositRate.value*$depositMonth.value/1200) * TARIFF.GENERAL)}</li>
+            <li>세후 수령액: ${Math.round($deposit.value*(1+$depositRate.value*$depositMonth.value/1200*(1 - TARIFF.GENERAL)))}</li>
         </ul>
         `;
         $savingsResult.innerHTML = `
         <ul>
             <li>원금: ${$savings.value}</li>
             <li>세전이자: ${($savings.value * $savingsMonth.value*(parseInt($savingsMonth.value)+1)/2 * $savingsRate.value/12*0.01)}</li>
-            <li>일반과세(15.4%): ${Math.round(($savings.value * $savingsMonth.value*(parseInt($savingsMonth.value)+1)/2 * $savingsRate.value/12*0.01) * 0.154)}</li>
-            <li>세후 수령액: ${($savings.value*$savingsMonth.value) + Math.round(($savings.value * $savingsMonth.value*(parseInt($savingsMonth.value)+1)/2 * $savingsRate.value/12*0.01)*0.846)}</li>
+            <li>일반과세(${TARIFF.GENERAL * 100}%): ${Math.round(($savings.value * $savingsMonth.value*(parseInt($savingsMonth.value)+1)/2 * $savingsRate.value/12*0.01) * TARIFF.GENERAL)}</li>
+            <li>세후 수령액: ${($savings.value*$savingsMonth.value) + Math.round(($savings.value * $savingsMonth.value*(parseInt($savingsMonth.value)+1)/2 * $savingsRate.value/12*0.01)*(1 - TARIFF.GENERAL))}</li>
         </ul>
         `;
     });
